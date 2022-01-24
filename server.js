dc03bf2f3109bab1b8b1861c5e5ed6233e7569cd
GaneshSonarSD <vinodsonar9777@gmail.com>
2022-01-24 15:07:50 +0530

    testing handlebars

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
+const { engine } = require('express-handlebars');
 
-app.get("/", (req, res)=>{
-    res.send("testing....")
-})
-app.listen(8080, ()=>{
-    console.log('server is running....');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views')
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function (req, res) {
+    res.render('home')
+});
+
+// custom 404 page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404')
+});
+
+// custom 500 page
+app.use((err, req, res, nex) => {
+    console.err(err.stack);
+    res.status(500);
+    res.render('500')
+});
+
+app.listen(app.get('port'), () => {
+    console.log('express is running on port', app.get('port'));
 })
